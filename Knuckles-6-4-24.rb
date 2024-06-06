a29e7d027162b4a534a69bbbe52e85198befc3ad
crestu <143304087+crestu@users.noreply.github.com>
2024-06-05 22:59:36 -0500

    Update Knuckles-6-5-24.rb
    removed lead, add hi hats, add snap, edit chop, slow release chords

--- Knuckles-6-4-24.rb
+++ Knuckles-6-4-24.rb
@@ -1,30 +1,33 @@
 use_sample_bpm :loop_compus, num_beats: 6
 
 live_loop :chop do
-  16.times do
-    sample :loop_compus, num_slices: 64, slice: (ring 0, pick, pick, 5).tick
+  2.times do
+    sample :loop_compus, amp: 1.5, num_slices: 32, slice: (ring 0, pick, pick, 5).tick
     sleep 0.25
   end
 end
 
-live_loop :lead do
-  use_synth :blade
-  use_synth_defaults release: 0.5, cutoff: 80, amp: 1.5
-  
-  notes = (ring :e2, :g2, :b2, :d3, :e3, :g3, :b3, :d4)
-  durations = (ring 0.5, 0.25, 0.75, 1, 0.25, 0.75, 0.25, 0.25)
-  
-  8.times do
-    play notes.tick, release: 0.5, cutoff: rrand(70, 100)
-    sleep durations.look
+live_loop :hats do
+  4.times do |i|
+    if i != 3
+      0.3
+      sample :hat_bdu, amp: 0.35
+    end
+    sleep 0.25
   end
+end
+
+live_loop :clap do
+  sleep 0.75
+  sample :perc_snap, amp: 0.5
+  sleep 0.25
 end
 
 live_loop :bass do
   use_synth :fm
   use_synth_defaults divisor: 2, depth: 1, release: 0.3, cutoff: 60
   
-  bass_notes = (ring :e2, :e2, :e3, :e2, :e2, :e2, :e3, :e3)
+  bass_notes = (ring :e2, :e2, :g3, :e2, :b2, :fs2, :b3, :e3)
   bass_durations = (ring 0.5, 0.25, 1, 0.5, 0.5,0.25, 0.5, 0.5)
   
   8.times do
@@ -34,14 +37,14 @@ live_loop :bass do
 end
 
 live_loop :chords do
-  use_synth :prophet
-  use_synth_defaults release: 1.5, cutoff: 80, amp: 1.2
+  use_synth :blade
+  use_synth_defaults release: 1.76, cutoff: 80, amp: 1.2, attack: 0.176, reverse: 1
   
   chords = [
-    chord(:e3, :m7),
-    chord(:g3, :major7),
-    chord(:a3, :madd9),
-    chord(:b3, :add9, num_octaves: 2).map { |note| note + 1 }
+    chord(:e2, :m7),
+    chord(:g2, :major7),
+    chord(:a2, :madd9),
+    chord(:b2, :add9, num_octaves: 2).map { |note| note + 1 }
   ]
   
   play chords.tick
