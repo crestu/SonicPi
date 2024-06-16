f8c84edc9494f8f9e67cec52fe3ac62e0c5557b8
crestu <143304087+crestu@users.noreply.github.com>
2024-06-16 14:02:12 -0500

    Update chao 6-15-24
    cowbell brazilian phonk tb, changed kick and 808 pattern

--- chao.rb
+++ chao.rb
@@ -1,38 +1,48 @@
-##| live_loop :ateohate do
-##|   kick_durations = (ring 0.75, 0.75, 0.75, 0.25, 0.75,0.75)
-##|   2.times do
-##|     with_fx :distortion, distort: 0.5 do
-##|       sample :bd_boom, amp: 1.5, release: 0.5
-##|       sleep kick_durations.tick
-##|     end
-##|   end
-##| end
+use_bpm 75
 
 live_loop :hihat_pattern do
-  sample :drum_cymbal_closed, amp: 0.4 if (spread 8, 8).tick
-  sleep 0.25
+  sample :drum_cymbal_closed, amp: 0.14
+  sleep 0.166
+end
+
+live_loop :clap do
+  sleep 0.5
+  sample :sn_generic, rpitch: 5, amp: 0.5
+  sleep 3
+  sample :sn_generic, rpitch: 5, amp: 0.5
+  sleep 0.5
 end
 
 live_loop :kick_pattern do
-  2.times do
-    kick_duration = (ring 0.5, 1.25, 0.25)
-    sample :bd_haus
+  4.times do
+    kick_duration = (ring 1.25, 0.5,0.25, 0.5, 1.25, 0.25)
+    sample :bd_haus,amp: 2, rpitch: 11
     sleep kick_duration.tick
   end
 end
 
 live_loop :synth do
   use_synth :prophet
-  with_synth_defaults cutoff: 60, amp: 0.1, release: 0.8 do
+  with_synth_defaults cutoff: 60, amp: 0.6, release: 0.8 do
     with_fx :distortion, distort: 0.5 do
-      synth_notes = (ring :e4, :ds4, :d4, :cs4, :c4, :b3, :as3, :a3, :gs3, :g3, :fs3, :f3)
-      synth_durations = (ring 0.33, 0.335, 0.335, 0.33, 0.335, 0.335, 0.33, 0.335, 0.335, 0.33, 0.335, 0.335)
+      synth_notes = (ring :e5, :ds5, :e5, :ds5, :g5, :fs5)
+      synth_durations = (ring 0.75, 0.75, 0.75, 0.75, 0.5, 0.5)
       
       note = synth_notes.tick
       duration = synth_durations.look
       
       play note
       sleep duration
+    end
+  end
+end
+
+live_loop :ateohate do
+  kick_durations = (ring 0.75, 1.25, 1.25, 0.75)
+  2.times do
+    with_fx :distortion, distort: 0.5 do
+      sample :bd_boom, amp: 2, release: 0.5, rpitch: -3.85
+      sleep kick_durations.tick
     end
   end
 end
