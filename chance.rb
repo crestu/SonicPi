f5d435f3215f7f287f203ee45df6e3ffcc6d8ed2
crestu <143304087+crestu@users.noreply.github.com>
2024-08-28 23:58:25 -0500

    Update chance.rb
    arranged bassline, fixed kick pattern, added synth, added fx, light mixing

--- chance.rb
+++ chance.rb
@@ -6,57 +6,80 @@ eight08 = "/Users/Creed/Downloads/Don Toliver - Life of a DON Deconstructed Drum
 
 live_loop :sample do
   2.times do
-    sample chance, num_slices: 16, slice: 0
-    sleep 2
-    sample chance, num_slices: 16, slice: 6
-    sleep 1.5
-    sample chance, num_slices: 16, slice: 9, start: 0, finish: 0.5
-    sleep 1
-    sample chance, num_slices: 16, slice: 1, start: 0, finish: 0.5
-    sleep 1.5
-    sample chance, num_slices: 16, slice: 8, start: 0, finish: 0.5
-    sleep 1
-    sample chance, num_slices: 16, slice: 8, start: 0, finish: 0.5
-    sleep 1
-  end
-end
-
-live_loop :kick do
-  8.times do
-    with_fx :eq, high: -1  do
-      sample kick
-      sleep 1
-      sample kick
-      sleep 1
-      sample kick
-      sleep 0.66
-      sample kick
-      sleep 0.66
-      sample kick
-      sleep 0.68
+    with_fx :eq, amp: 0.7 do
+      with_fx :reverb, mix: 0.3 do
+        sample chance, num_slices: 16, slice: 0
+        sleep 2
+        sample chance, num_slices: 16, slice: 6
+        sleep 1.5
+        sample chance, num_slices: 16, slice: 9, start: 0, finish: 0.5
+        sleep 1
+        sample chance, num_slices: 16, slice: 1, start: 0, finish: 0.5
+        sleep 1.5
+        sample chance, num_slices: 16, slice: 8, start: 0, finish: 0.5
+        sleep 1
+        sample chance, num_slices: 16, slice: 8, start: 0, finish: 0.5
+        sleep 1
+      end
     end
   end
 end
 
-##| live_loop :eight08 do
-##|   8.times do
-##|     with_fx :eq, high: -1  do
-##|       sample eight08, pitch: 8, start: 0, finish: 0.5
-##|       sleep 1
-##|       sample eight08, pitch: -4, start: 0, finish: 0.5
-##|       sleep 1
-##|       sample eight08, pitch: -4, start: 0, finish: 0.5
-##|       sleep 1
-##|       sample eight08, pitch: 8, start: 0, finish: 0.5
-##|       sleep 1
-##|       sample eight08, pitch: 8, start: 0, finish: 0.5
-##|       sleep 1
-##|       sample eight08, pitch: -4, start: 0, finish: 0.5
-##|       sleep 1
-##|       sample eight08, pitch: 8, start: 0, finish: 0.5
-##|       sleep 1
-##|       sample eight08, pitch: -4, start: 0, finish: 0.5
-##|       sleep 1
-##|     end
-##|   end
-##| end
+live_loop :kick do
+  2.times do
+    with_fx :eq, high: -1, amp: 1.4  do
+      sample kick
+      sleep 1
+      sample kick
+      sleep 1
+      sample kick
+      sleep 0.75
+      sample kick
+      sleep 0.75
+      sample kick
+      sleep 0.5
+    end
+  end
+end
+
+live_loop :eight08 do
+  1.times do
+    with_fx :eq, amp: 0.7 do
+      with_fx :lpf, mix: 0.5 do
+        sample eight08, rpitch: 8, start: 0, finish: 0.25
+        sleep 1
+        sample eight08, rpitch: -4, start: 0, finish: 0.25
+        sleep 1
+        sample eight08, rpitch: -4, start: 0, finish: 0.25
+        sleep 0.5
+        sample eight08, rpitch: 8, start: 0, finish: 0.25
+        sleep 1
+        sample eight08, rpitch: 8, start: 0, finish: 0.25
+        sleep 1
+        sample eight08, rpitch: -4, start: 0, finish: 0.25
+        sleep 1.5
+        sample eight08, rpitch: 8, start: 0, finish: 0.25
+        sleep 1
+        sample eight08, rpitch: -4, start: 0, finish: 0.25
+        sleep 1
+      end
+    end
+  end
+end
+
+live_loop :synth do
+  4.times do
+    with_fx :eq, amp: 0.075 do
+      with_fx :lpf, mix: 0.6 do
+        use_synth :supersaw
+        
+        play :gs3, release: 5
+        sleep 4
+        play :b4, release: 3
+        sleep 2
+        play :as4, release: 3
+        sleep 2
+      end
+    end
+  end
+end
